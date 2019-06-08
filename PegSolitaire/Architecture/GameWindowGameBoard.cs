ca4e8ef7426eacd12a48182a0ecf4fe5d4440f27
GodmotherMorrison <emptyset2017@gmail.com>
2019-06-09 01:31:16 +0500

    refactoring

---
+++ PegSolitaire/Architecture/GameWindowGameBoard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace PegSolitaire.Architecture
+{
+    public partial class GameWindow : Form
+    {
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Game.Game.UpdateBoard(e.Location, (Point)pictureBoxGameBoard.Size);
+            pictureBoxGameBoard.Image = Game.Game.Display;
+
+            if (!Game.Game.IsOver()) return;
+
+            if (Game.Game.IsWin())
+                using (var g = Graphics.FromImage(Game.Game.Display))
+                {
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    var cell = new Rectangle(0, Game.Game.Display.Height / 3, Game.Game.Display.Width,
+                        Game.Game.Display.Height / 3);
+                    g.DrawImage(Images.YouWon, cell);
+                }
+            else
+                using (var g = Graphics.FromImage(Game.Game.Display))
+                {
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    var cell = new Rectangle(0, Game.Game.Display.Height / 3, Game.Game.Display.Width,
+                        Game.Game.Display.Height / 3);
+                    g.DrawImage(Images.GameOver, cell);
+                }
+        }
+
+        private void PictureBox1_SizeChanged(object sender, EventArgs e)
+        {
+            Game.Game.SizeOfDisplay = (pictureBoxGameBoard.Width > pictureBoxGameBoard.Height) ? pictureBoxGameBoard.Height : pictureBoxGameBoard.Width;
+        }
+
+    }
+}
---
+++ PegSolitaire/Architecture/GameWindowMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PegSolitaire.Architecture
+{
+    public partial class GameWindow : Form
+    {
+        private void PictureBoxPlay_Click(object sender, EventArgs e)
+        {
+            ShowPanel(panelGame);
+        }
+
+        private void PictureBoxNewGame_Click(object sender, EventArgs e)
+        {
+            pictureBoxGameBoard.Image = null;
+            Game.Game.CreateBoard(Game.Game.StringBoard);
+            pictureBoxGameBoard.Image = Game.Game.GetDrawnBoard();
+            ShowPanel(panelGame);
+        }
+        private void PictureBoxSelectField_Click(object sender, EventArgs e)
+        {
+            ShowPanel(panelBoards);
+        }
+
+        private void PictureBoxExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
